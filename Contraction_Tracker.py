a41ca1d3868c9fdafd1a51d91744513798d1565f
cEdw2448 <cedw2411@gmail.com>
2025-08-30 19:06:05 -0600

    Cronometros, sin límite de tiempo ni promedio

--- Contraction_Tracker.py
+++ Contraction_Tracker.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.clock import Clock
 import time
+from kivy.logger import Logger
 
 id_contraction = 0
 promedio = []
@@ -15,7 +16,7 @@ class StopwatchApp(App):
     def build(self):
         self.root_layout = BoxLayout(orientation='vertical', padding=20)
 
-        self.time_label = Label(text='00:00:00.00', font_size='50sp')
+        self.time_label = Label(text='00:00.00', font_size='50sp')
         self.root_layout.add_widget(self.time_label)
 
         self.buttons_layout = BoxLayout(spacing=10)
@@ -25,52 +26,70 @@ class StopwatchApp(App):
 
         self.start_button.bind(on_press=self.start_timer)
         self.end_button.bind(on_press=self.end_contraction)
-        self.reset_button.bind(on_press=self.reset_timer)
+        self.reset_button.bind(on_press=self.reset)
 
         self.buttons_layout.add_widget(self.start_button)
         self.buttons_layout.add_widget(self.end_button)
         self.buttons_layout.add_widget(self.reset_button)
         self.root_layout.add_widget(self.buttons_layout)
 
+        self.remaining_time=0.0
         self.start_time = None
         self.elapsed_time = 0.0
-        self.timer_event = None
+        self.timer_event1 = None
 
         return self.root_layout
     
     def new_contraction(self):
         global id_contraction, promedio
         id_contraction = id_contraction + 1
-        promedio.append(self.elapsed_time)
+        i=id_contraction//2
+        promedio.insert(i,self.elapsed_time)
 
-        self.time_label = Label(text="Contraction "+str(id_contraction//2), font_size='50sp')
-        self.root_layout.add_widget(self.time_label)
+        self.time_label_n = Label(text="Contraction "+str(i), font_size='50sp')
+        self.root_layout.add_widget(self.time_label_n)
         self.start_time = time.time()
         self.elapsed_time = 0.0
         self.timer_event = None
         return self.root_layout
 
-    def update_time(self, dt):
+    def update_time(self, dt,id_clock):
         """Actualiza el temporizador y la etiqueta."""
+        #while self.remaining_time <=10:
         if self.start_time:
             self.elapsed_time = time.time() - self.start_time
-        else:
-            # Si el cronómetro no se ha iniciado, mantiene el tiempo cero
-            self.elapsed_time = 0.0
-
+            
+        if self.start_time1:
+            self.remaining_time = time.time() - self.start_time1
+            
+        if id_clock==1:
         # Formatear el tiempo
-        minutes, seconds = divmod(self.elapsed_time, 60)
-        hours, minutes = divmod(minutes, 60)
-        time_string = f'{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}.{int(self.elapsed_time * 100 % 100):02d}'
-        self.time_label.text = time_string
-
+            minutes, seconds = divmod(self.remaining_time, 60)
+            time_string = f'{int(minutes):02d}:{int(seconds):02d}.{int(self.remaining_time * 100 % 100):02d}'
+            self.time_label.text = time_string
+            
+        if id_clock==2:
+            # Formatear el tiempo
+            minutes, seconds = divmod(self.elapsed_time, 60)
+            time_string = f'{int(minutes):02d}:{int(seconds):02d}.{int(self.elapsed_time * 100 % 100):02d}'
+            self.time_label_n.text = time_string
+        #else:
+         #   for i in promedio:
+          #      n=n+1
+           #     suma=i+suma
+            #prom=suma/n
+            #Logger.info(prom)
+            #time.sleep(5)
+            
     def start_timer(self, instance):
         """Inicia o reanuda el cronómetro."""
-        if self.timer_event is None: # Solo inicia si no está ya corriendo
-            time.time()
-            self.new_contraction()
-            self.start_time = time.time() # Restaura el tiempo si se había pausado
-            self.timer_event = Clock.schedule_interval(self.update_time, 0.01) # Actualiza cada 0.01 segundos
+        
+        self.timer_event1 = Clock.schedule_interval(lambda dt: self.update_time(dt,1), 0.01) # Actualiza cada 0.01 segundos
+        self.start_time1=time.time()-self.remaining_time
+
+        self.new_contraction()         
+        self.start_time = time.time() # Restaura el tiempo si se había pausado
+        self.timer_event = Clock.schedule_interval(lambda dt: self.update_time(dt,2), 0.01) # Actualiza cada 0.01 segundos     
 
     def end_contraction(self, instance):
         """Pausa el cronómetro."""
@@ -79,9 +98,13 @@ class StopwatchApp(App):
             # Mantenemos slf.start_time en None para que la siguiente actualización se base en self.elapsed_time
             self.new_contraction()
 
-    def reset_timer(self, instance):
+    def reset(self, instance):
         """Reinicia el cronómetro a cero."""
-        self.update_time(None) # Actualiza la etiqueta a 00:00:00.00
+        self.remaining_time,self.start_time1=0,0
+        self.root.clear_widgets()
+        self.stop()
+        self.__init__()
+        self.run()
 
 if __name__ == '__main__':
     StopwatchApp().run()
